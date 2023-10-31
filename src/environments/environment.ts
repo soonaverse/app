2a10410cdc0aed946d06cdaad1f90373c86ba8f2
Adam <adam@unchained.work>
2023-10-31 16:02:53 -0700

    added build5 token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,5 @@ export const environment = {
     key: '5c053a0370395f0fe8cf7ce9e5a21a72',
   },
   soonaversePlaceholder: 'https://soonaverse.com/favicon.ico',
+  build5Token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
 };
